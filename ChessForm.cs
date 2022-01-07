8a75258595cceb8a3deca08b2bb4b7af3c183ed0
CYC0630 <95112132+CYC0630@users.noreply.github.com>
2022-01-08 02:39:21 +0800

    First Upload.
    Upload the whole solution of C# form about chess game.

---
+++ ChessForm.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Chess;
+
+namespace Final
+{
+    public partial class ChessForm : Form
+    {
+        private readonly ChessPieces[,] table; //棋盤
+        private readonly Button[,] buttons; //按鈕
+        private readonly List<Place> mark_as_green; //記錄有哪些格數被標為綠色 可以走
+        private bool[,] can_move_point; //棋盤上有哪些點可以走
+        private bool move_stage; //正在試圖移動棋子
+        private PieceColor now_color; //現在輪到誰了
+        private ChessPieces target_piece; //要動的棋子
+
+        private const int SIZE_X = 8;
+        private const int SIZE_Y = 8;
+
+        public ChessForm()
+        {
+            InitializeComponent();
+
+            table = new ChessPieces[SIZE_X, SIZE_Y]
+            {
+                {
+                    new Rook(0, 0, PieceColor.black),
+                    new Pawn(0, 1, PieceColor.black),
+                    new None(0, 2, PieceColor.none),
+                    new None(0, 3, PieceColor.none),
+                    new None(0, 4, PieceColor.none),
+                    new None(0, 5, PieceColor.none),
+                    new Pawn(0, 6, PieceColor.white),
+                    new Rook(0, 7, PieceColor.white)
+                },
+                {
+                    new Knight(1, 0, PieceColor.black),
+                    new Pawn(1, 1, PieceColor.black),
+                    new None(1, 2, PieceColor.none),
+                    new None(1, 3, PieceColor.none),
+                    new None(1, 4, PieceColor.none),
+                    new None(1, 5, PieceColor.none),
+                    new Pawn(1, 6, PieceColor.white),
+                    new Knight(1, 7, PieceColor.white)
+                },
+                {
+                    new Bishop(2, 0, PieceColor.black),
+                    new Pawn(2, 1, PieceColor.black),
+                    new None(2, 2, PieceColor.none),
+                    new None(2, 3, PieceColor.none),
+                    new None(2, 4, PieceColor.none),
+                    new None(2, 5, PieceColor.none),
+                    new Pawn(2, 6, PieceColor.white),
+                    new Bishop(2, 7, PieceColor.white)
+                },
+                {
+                    new Queen(3, 0, PieceColor.black),
+                    new Pawn(3, 1, PieceColor.black),
+                    new None(3, 2, PieceColor.none),
+                    new None(3, 3, PieceColor.none),
+                    new None(3, 4, PieceColor.none),
+                    new None(3, 5, PieceColor.none),
+                    new Pawn(3, 6, PieceColor.white),
+                    new Queen(3, 7, PieceColor.white)
+                },
+                {
+                    new King(4, 0, PieceColor.black),
+                    new Pawn(4, 1, PieceColor.black),
+                    new None(4, 2, PieceColor.none),
+                    new None(4, 3, PieceColor.none),
+                    new None(4, 4, PieceColor.none),
+                    new None(4, 5, PieceColor.none),
+                    new Pawn(4, 6, PieceColor.white),
+                    new King(4, 7, PieceColor.white)
+                },
+                {
+                    new Bishop(5, 0, PieceColor.black),
+                    new Pawn(5, 1, PieceColor.black),
+                    new None(5, 2, PieceColor.none),
+                    new None(5, 3, PieceColor.none),
+                    new None(5, 4, PieceColor.none),
+                    new None(5, 5, PieceColor.none),
+                    new Pawn(5, 6, PieceColor.white),
+                    new Bishop(5, 7, PieceColor.white)
+                },
+                {
+                    new Knight(6, 0, PieceColor.black),
+                    new Pawn(6, 1, PieceColor.black),
+                    new None(6, 2, PieceColor.none),
+                    new None(6, 3, PieceColor.none),
+                    new None(6, 4, PieceColor.none),
+                    new None(6, 5, PieceColor.none),
+                    new Pawn(6, 6, PieceColor.white),
+                    new Knight(6, 7, PieceColor.white)
+                },
+                {
+                    new Rook(7, 0, PieceColor.black),
+                    new Pawn(7, 1, PieceColor.black),
+                    new None(7, 2, PieceColor.none),
+                    new None(7, 3, PieceColor.none),
+                    new None(7, 4, PieceColor.none),
+                    new None(7, 5, PieceColor.none),
+                    new Pawn(7, 6, PieceColor.white),
+                    new Rook(7, 7, PieceColor.white)
+                }
+            };
+
+            buttons = new Button[,]//[SIZE_X, SIZE_Y]
+            {
+                { A8,A7,A6,A5,A4,A3,A2,A1 },
+                { B8,B7,B6,B5,B4,B3,B2,B1 },
+                { C8,C7,C6,C5,C4,C3,C2,C1 },
+                { D8,D7,D6,D5,D4,D3,D2,D1 },
+                { E8,E7,E6,E5,E4,E3,E2,E1 },
+                { F8,F7,F6,F5,F4,F3,F2,F1 },
+                { G8,G7,G6,G5,G4,G3,G2,G1 },
+                { H8,H7,H6,H5,H4,H3,H2,H1 }
+            };
+
+            mark_as_green = new List<Place>();
+
+            move_stage = false; //一開始當然不在移動階段 是在選擇棋子階段
+
+            now_color = PieceColor.white; //規則是白棋先走
+        }
+
+        private void Checkmate(PieceColor winner)
+        {
+
+        }
+
+        private void OnPieceClick(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            //每個棋子都有他們的tag 記錄這個棋子的位址
+            //左上角的是00
+            //右上角的是70
+            //左下角的是07
+            //右下角的是77
+            int x = int.Parse(button.Tag.ToString());
+            int y = x % 10;
+            x /= 10;
+
+            ChessPieces piece = table[x, y]; //取得點擊的棋子
+
+            if (move_stage) //落子階段
+            {
+                foreach (Place p in mark_as_green)
+                    buttons[p.x, p.y].BackColor = p.color; //恢復棋盤底色
+
+                //mark_as_green.Find()
+
+                bool checkmate = false;
+                foreach (Place p in mark_as_green) //走訪所有合法路徑
+                {
+                    if (x == p.x && y == p.y) //走了合法路徑
+                    {
+                        int last_x = target_piece.X; //選子階段點的棋子 X
+                        int last_y = target_piece.Y; //選子階段點的棋子 Y
+
+                        checkmate = piece is King; //如果被吃掉的是國王
+
+                        table[x, y] = table[last_x, last_y]; //棋子走過去
+                        buttons[x, y].BackgroundImage = buttons[last_x, last_y].BackgroundImage; //圖像轉移過去
+
+                        table[last_x, last_y] = new None(last_x, last_y, PieceColor.none); //原本的變成無棋子
+                        buttons[last_x, last_y].BackgroundImage = null; //原本的變成無圖像
+
+                        break; //不須再比較 離開for迴圈
+                    }
+                }
+
+                if (checkmate) //國王被吃掉
+                    Checkmate(now_color);
+
+                now_color = now_color == PieceColor.white ? PieceColor.black : PieceColor.white; //切換顏色
+                move_stage = false; //回到選子階段
+
+            }
+            else //這個點擊是為了選擇棋子
+            {
+                if (piece.Color == PieceColor.none) //該點沒有棋子 顏色是none
+                    return;
+
+                //輪到白棋卻點了黑棋
+                if (now_color == PieceColor.white && piece.Color == PieceColor.black)
+                    return;
+
+                //輪到黑棋卻點了白棋
+                if (now_color == PieceColor.black && piece.Color == PieceColor.white)
+                    return;
+
+                target_piece = piece; //紀錄下是這個棋子要動
+                can_move_point = piece.AbleToMove(table); //找出可以移動的點
+
+                mark_as_green.Clear(); //清空list內的綠色標記
+                for (int i = 0; i < SIZE_X; i++) //走訪棋盤上所有可以走的位置
+                {
+                    for (int j = 0; j < SIZE_Y; j++)
+                    {
+                        if (can_move_point[i, j])
+                        {
+                            mark_as_green.Add(new Place(i, j, buttons[i, j].BackColor)); //將綠色標記加入list
+                            buttons[i, j].BackColor = Color.Green; //將棋盤上可以走的地方變成綠色
+                        }
+                    }
+                }
+
+                move_stage = true; //進入落子階段
+            }
+        }
+
+        private void MenuRestart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MenuCancelSelect_Click(object sender, EventArgs e)
+        {
+            if (move_stage)
+            {
+                foreach (Place p in mark_as_green)
+                    buttons[p.x, p.y].BackColor = p.color; //恢復棋盤底色
+                move_stage = false;
+            }
+        }
+    }
+
+    internal class Place
+    {
+        public Place(int set_x, int set_y, Color set_color)
+        {
+            x = set_x;
+            y = set_y;
+            color = set_color;
+        }
+
+        public int x;
+        public int y;
+        internal Color color;
+    }
+}
---
+++ ChessPieces.cs
@@ -0,0 +1,383 @@
+﻿namespace Chess
+{
+    public enum PieceColor
+    {
+        white,
+        black,
+        none //沒有棋子
+    }
+
+    public abstract class ChessPieces
+    {
+        protected int x, y;
+        protected PieceColor color;
+
+        public int X => x;
+        public int Y => y;
+        public PieceColor Color => color;
+
+        public ChessPieces(int set_x, int set_y, PieceColor set_color) =>
+            (x, y, color) = (set_x, set_y, set_color);
+
+        //可以走
+        public abstract bool[,] AbleToMove(ChessPieces[,] table);
+
+        //轉字串
+        public override string ToString() =>
+            (color == PieceColor.white ? "W" : "B") + GetType().Name;
+    }
+
+    //國王
+    public class King : ChessPieces
+    {
+        private bool can_castle; //可否入堡
+
+        public King(int set_x, int set_y, PieceColor set_color)
+            : base(set_x, set_y, set_color)
+        {
+            can_castle = false;
+        }
+
+        public override bool[,] AbleToMove(ChessPieces[,] table)
+        {
+            bool[,] able_to_move = Tools.GenerateBoolArray();
+
+            //走訪自身為中心3x3
+            for (int i = x - 1; i < x + 1; i++)
+                for (int j = y - 1; j < y + 1; j++)
+                    if (Tools.InTable(i, j) && (i != x || j != y)) //在棋盤內 且不可和棋子本身同一格
+                        able_to_move[i, j] = table[i, j].Color != color; //如果顏色和我的顏色不一樣 (沒有棋子是none) 就可以走
+
+            if (can_castle) //如果可以入堡
+                able_to_move[x - 2, y] = able_to_move[x + 2, y] = true;
+
+            return able_to_move;
+        }
+
+        public bool CanCastle
+        {
+            set => can_castle = value;
+        }
+    }
+
+    //王后
+    public class Queen : ChessPieces
+    {
+        public Queen(int set_x, int set_y, PieceColor set_color)
+            : base(set_x, set_y, set_color) { }
+
+        public override bool[,] AbleToMove(ChessPieces[,] table)
+        {
+            bool[,] able_to_move = Tools.GenerateBoolArray();
+
+            int i, j; //走訪要用
+            //向左上走
+            for (i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--)
+            {
+                if (table[i, j].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[i, j] = true; //可以走
+                if (table[i, j].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+            //向上走
+            for (j = y - 1; j >= 0; j--)
+            {
+                if (table[x, j].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[x, j] = true; //可以走
+                if (table[x, j].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+            //向右上走
+            for (i = x + 1, j = y - 1; i <= 7 && j >= 0; i++, j--)
+            {
+                if (table[i, j].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[i, j] = true; //可以走
+                if (table[i, j].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+            //向右走
+            for (i = x + 1; i <= 7; i++)
+            {
+                if (table[i, y].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[i, y] = true; //可以走
+                if (table[i, y].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+            //向右下走
+            for (i = x + 1, j = y + 1; i <= 7 && j <= 7; i++, j++)
+            {
+                if (table[i, j].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[i, j] = true; //可以走
+                if (table[i, j].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+            //向下走
+            for (j = y + 1; j <= 7; j++)
+            {
+                if (table[x, j].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[x, j] = true; //可以走
+                if (table[x, j].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+            //向左下走
+            for (i = x - 1, j = y + 1; i >= 0 && j <= 7; i--, j++)
+            {
+                if (table[i, j].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[i, j] = true; //可以走
+                if (table[i, j].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+            //向左走
+            for (i = x - 1; i >= 0; i--)
+            {
+                if (table[i, y].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[i, y] = true; //可以走
+                if (table[i, y].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+
+            return able_to_move;
+        }
+    }
+
+    public class Bishop : ChessPieces
+    {
+        public Bishop(int set_x, int set_y, PieceColor set_color)
+            : base(set_x, set_y, set_color) { }
+
+        public override bool[,] AbleToMove(ChessPieces[,] table)
+        {
+            bool[,] able_to_move = Tools.GenerateBoolArray();
+
+            int i, j; //走訪要用
+            //向左上走
+            for (i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--)
+            {
+                if (table[i, j].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[i, j] = true; //可以走
+                if (table[i, j].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+            //向右上走
+            for (i = x + 1, j = y - 1; i <= 7 && j >= 0; i++, j--)
+            {
+                if (table[i, j].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[i, j] = true; //可以走
+                if (table[i, j].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+            //向右下走
+            for (i = x + 1, j = y + 1; i <= 7 && j <= 7; i++, j++)
+            {
+                if (table[i, j].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[i, j] = true; //可以走
+                if (table[i, j].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+            //向左下走
+            for (i = x - 1, j = y + 1; i >= 0 && j <= 7; i--, j++)
+            {
+                if (table[i, j].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[i, j] = true; //可以走
+                if (table[i, j].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+
+            return able_to_move;
+        }
+    }
+
+    public class Knight : ChessPieces
+    {
+        public Knight(int set_x, int set_y, PieceColor set_color)
+            : base(set_x, set_y, set_color) { }
+
+        public override bool[,] AbleToMove(ChessPieces[,] table)
+        {
+            bool[,] able_to_move = Tools.GenerateBoolArray();
+
+            int i, j;
+            //左上左
+            i = x - 2;
+            j = y - 1;
+            if (Tools.InTable(i, j))
+                able_to_move[i, j] = table[i, j].Color != color; //如果顏色和我的顏色不一樣 (沒有棋子是none)
+            //左上上
+            i = x - 1;
+            j = y - 2;
+            if (Tools.InTable(i, j))
+                able_to_move[i, j] = table[i, j].Color != color;
+            //右上上
+            i = x + 1;
+            j = y - 2;
+            if (Tools.InTable(i, j))
+                able_to_move[i, j] = table[i, j].Color != color;
+            //右上右
+            i = x + 2;
+            j = y - 1;
+            if (Tools.InTable(i, j))
+                able_to_move[i, j] = table[i, j].Color != color;
+            //右下右
+            i = x + 2;
+            j = y + 1;
+            if (Tools.InTable(i, j))
+                able_to_move[i, j] = table[i, j].Color != color;
+            //右下下
+            i = x + 1;
+            j = y + 2;
+            if (Tools.InTable(i, j))
+                able_to_move[i, j] = table[i, j].Color != color;
+            //左下下
+            i = x - 1;
+            j = y + 2;
+            if (Tools.InTable(i, j))
+                able_to_move[i, j] = table[i, j].Color != color;
+            //左下左
+            i = x - 2;
+            j = y + 1;
+            if (Tools.InTable(i, j))
+                able_to_move[i, j] = table[i, j].Color != color;
+
+            return able_to_move;
+        }
+    }
+
+    public class Rook : ChessPieces
+    {
+        public Rook(int set_x, int set_y, PieceColor set_color)
+            : base(set_x, set_y, set_color) { }
+
+        public override bool[,] AbleToMove(ChessPieces[,] table)
+        {
+            bool[,] able_to_move = Tools.GenerateBoolArray();
+
+            int i, j;
+            //向左走
+            for (i = x - 1; i >= 0; i--)
+            {
+                if (table[i, y].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[i, y] = true; //可以走
+                if (table[i, y].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+            //向上走
+            for (j = y - 1; j >= 0; j--)
+            {
+                if (table[x, j].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[x, j] = true; //可以走
+                if (table[x, j].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+            //向右走
+            for (i = x + 1; i <= 7; i++)
+            {
+                if (table[i, y].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[i, y] = true; //可以走
+                if (table[i, y].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+            //向下走
+            for (j = y + 1; j <= 7; j++)
+            {
+                if (table[x, j].Color != color) //如果顏色和我的顏色不一樣 (沒有棋子是none)
+                    able_to_move[x, j] = true; //可以走
+                if (table[x, j].Color != PieceColor.none) //有棋子擋著
+                    break; //結束這個迴圈
+            }
+
+            return able_to_move;
+        }
+    }
+
+    public class Pawn : ChessPieces
+    {
+        private bool can_jump_two;
+
+        public Pawn(int set_x, int set_y, PieceColor set_color)
+            : base(set_x, set_y, set_color)
+        {
+            can_jump_two = true;
+        }
+
+        public override bool[,] AbleToMove(ChessPieces[,] table)
+        {
+            bool[,] able_to_move = Tools.GenerateBoolArray();
+
+            if (color == PieceColor.white)
+            {
+                //如果前面一格是存在的
+                if (Tools.InTable(x, y - 1))
+                {
+                    //如果還沒走過第一步
+                    if (can_jump_two) //檢查前兩格 第二格 = (第一格 = 第一格是不是空的) && 第二格是不是空的
+                        able_to_move[x, y - 2] = (able_to_move[x, y - 1] = table[x, y - 1].Color == PieceColor.none) && (table[x, y - 2].Color == PieceColor.none);
+                    else //檢查前面一格
+                        able_to_move[x, y - 1] = table[x, y - 1].Color == PieceColor.none;
+                }
+
+                if (Tools.InTable(x - 1, y - 1)) //左前方有敵人
+                    able_to_move[x - 1, y - 1] = table[x - 1, y - 1].Color == PieceColor.black;
+
+                if (Tools.InTable(x + 1, y - 1)) //右前方有敵人
+                    able_to_move[x + 1, y - 1] = table[x + 1, y - 1].Color == PieceColor.black;
+            }
+            else //if (color == PieceColor.black)
+            {
+                //如果前面一格是存在的
+                if (Tools.InTable(x, y + 1))
+                {
+                    //如果還沒走過第一步
+                    if (can_jump_two) //檢查前兩格 第二格 = (第一格 = 第一格是不是空的) && 第二格是不是空的
+                        able_to_move[x, y + 2] = (able_to_move[x, y + 1] = table[x, y + 1].Color == PieceColor.none) && (table[x, y + 2].Color == PieceColor.none);
+                    else //檢查前面一格
+                        able_to_move[x, y + 1] = table[x, y + 1].Color == PieceColor.none;
+                }
+
+                if (Tools.InTable(x - 1, y + 1)) //左前方有敵人
+                    able_to_move[x - 1, y + 1] = table[x - 1, y + 1].Color == PieceColor.black;
+
+                if (Tools.InTable(x + 1, y + 1)) //右前方有敵人
+                    able_to_move[x + 1, y + 1] = table[x + 1, y + 1].Color == PieceColor.black;
+
+            }
+
+            return able_to_move;
+        }
+
+        public bool CanJumpTwo
+        {
+            set => can_jump_two = value;
+        }
+    }
+
+    public class None : ChessPieces
+    {
+        public None(int set_x, int set_y, PieceColor set_color)
+            : base(set_x, set_y, set_color) { }
+
+        public override bool[,] AbleToMove(ChessPieces[,] table)
+        {
+            return null;
+        }
+    }
+
+    internal class Tools
+    {
+        //產生棋盤boolean陣列
+        //儲存每個點是否可以下
+        //預設全為false
+        internal static bool[,] GenerateBoolArray()
+        {
+            bool[,] new_array = new bool[8, 8];
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                    new_array[i, j] = false;
+            return new_array;
+        }
+
+        //檢測傳入的座標是否在棋盤內
+        //0 <= x <= 7 && 0 <= y <= 7
+        internal static bool InTable(int x, int y) =>
+            x >= 0 && x <= 7 && y >= 0 && y <= 7;
+    }
+}
